a914e590b4b291fb964783e6c239c0de762b2523
Homer Quan <homerquan@gmail.com>
2025-02-04 22:17:01 -0500

    ok

--- README.md
+++ README.md
@@ -3,10 +3,5 @@ A demo of SLM on robotic control
 
 ## How to demo online
 
-1. On mac, star the webots with web stream:
-`/Applications/Webots.app/Contents/MacOS/webots --stream`
-
-2. Open `http://localhost:1234/index.html`
-
-
-3. update controller `/Applications/Webots.app/Contents/projects/robots/kuka/youbot/controllers/youbot/`
+1. `sh start.sh`
+2. open `http://localhost:8080` on a browser
---
+++ www/setup_viewer.js
@@ -0,0 +1,53 @@
+// Check if the device is mobile so that the mobile CSS can be loaded.
+const mobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+if (mobileDevice) {
+  const head = document.getElementsByTagName('head')[0];
+  const mobileCss = document.createElement('link');
+  mobileCss.setAttribute('rel', 'stylesheet');
+  mobileCss.setAttribute('type', 'text/css');
+  mobileCss.setAttribute('href', 'https://www.cyberbotics.com/wwi/R2023b/css/wwi_mobile.css');
+  head.appendChild(mobileCss);
+}
+
+/**
+ * Automatically connect to the WebSocket on page load.
+ * The Webots view is set to use the "x3d" streaming mode.
+ */
+function autoConnect() {
+  const defaultThumbnail = 'https://cyberbotics.com/wwi/R2023b/images/loading/default_thumbnail.png';
+  const streamingMode = "x3d"; // Streaming mode set to "x3d" as requested.
+  const webotsView = document.getElementsByTagName('webots-view')[0];
+
+  // Set event handlers (if needed, you can customize these further)
+  webotsView.onready = onConnect;
+  webotsView.ondisconnect = onDisconnect;
+
+  // Connect to the WebSocket (adjust the URL if necessary)
+  webotsView.connect("ws://localhost:1234", streamingMode, false, mobileDevice, -1, defaultThumbnail);
+}
+
+function onConnect() {
+  console.log("Webots view connected.");
+  // You may add further actions on connect here.
+}
+
+function onDisconnect() {
+  console.log("Webots view disconnected.");
+  // You may add further actions on disconnect here.
+}
+
+// Set up the page once it loads.
+window.addEventListener('load', function() {
+  // Automatically connect the Webots view.
+  autoConnect();
+
+  // Set up the "Send" button behavior.
+  const sendButton = document.getElementById('sendButton');
+  const bigInput = document.getElementById('bigInput');
+
+  sendButton.onclick = function() {
+    const text = bigInput.value;
+    // TODO: Add your code here to send the text somewhere (for example, to the WebSocket).
+    console.log("Send button clicked with text:", text);
+  };
+});
